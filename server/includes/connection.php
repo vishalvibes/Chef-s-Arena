15ba50c379abc4c00b488cad395ad6c13a9274dc
Vishal Pratap Singh <Vishal Pratap Singh>
2020-05-01 02:22:23 +0530

    deleted everything

--- server/includes/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "my_user";
-$dbPassword = "password";
-$dbName = "testreact";
-
-$db = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
