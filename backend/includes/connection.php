8b893e5f8a87f118b8de65386e41b278103fd515
Vishal Pratap Singh <Vishal Pratap Singh>
2020-05-04 03:19:11 +0530

    clearing

--- backend/includes/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = $_ENV["HTTP_DB_SERVER"];
-$dbUsername = "my_user";
-$dbPassword = "password";
-$dbName = "testreact";
-
-$db = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
